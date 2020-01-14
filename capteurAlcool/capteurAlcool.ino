ef8ab03a3666cab39a3736220462c7e79671e5e5
fikrisaman <36538207+fikrisaman@users.noreply.github.com>
2020-01-14 22:11:20 +0100

    Add files via upload
    Ces capteurs donnent le ratio du gaz environnant

---
+++ capteurAlcool/capteurAlcool.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+
+#define heaterSelPin A1 //  pin SCL
+#define RS_air 1.24
+//  DAT = pin A0
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(heaterSelPin,OUTPUT);   // set the heaterSelPin as digital output.
+    digitalWrite(heaterSelPin,LOW); // Start to heat the sensor
+}
+
+void loop() {
+
+    float sensor_volt;
+    float RS_gas; // Get value of RS in a GAS
+    float ratio; // Get ratio RS_GAS/RS_air
+    //int sensorValue = Robot.analogRead(analogPin);
+    int sensorValue = analogRead(A0);
+    sensor_volt=(float)sensorValue/1024*5.0;
+    RS_gas = sensor_volt/(5.0-sensor_volt); // omit *R16
+
+  /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+    ratio = RS_gas/RS_air;  // ratio = RS/R0
+  /*-----------------------------------------------------------------------*/
+
+//    Serial.print("sensor_volt = ");
+//    Serial.println(sensor_volt);
+//    Serial.print("RS_ratio = ");
+//    Serial.println(RS_gas);
+    Serial.print("Rs/R0 = ");
+    Serial.println(ratio);
+
+    Serial.print("\n\n");
+    delay(1000);
+}
